496cf7fe4e0a01b555f61ca3548b02e2ee2c2c9b
Loyiso Ngqwebo <loyiso.ngqwebo@quirk.biz>
2016-05-21 14:55:34 +0200

    site update

--- modules/apidisplay/src/Controller/ApidisplayController.php
+++ modules/apidisplay/src/Controller/ApidisplayController.php
@@ -1,28 +1,36 @@
 <?php
 
-namespace Drupal\apidisplay\src\Controller;
+namespace Drupal\apidisplay\Controller;
 
   use Symfony\Component\HttpFoundation\Request;
 
 
   class ApidisplayController  {
 
-    public function index(Request $request){
+    public function index (Request $request){
 
       $client = new \GuzzleHttp\Client;
       $response = $client->request('GET', 'https://github.com/loyison');
+
       $data = json_decode($response->getBody()->getContents(), true);
+      return [
+        'avatar_url'  => $data['avatar_url'],
+        'name'        => $data['name'],
+        'login'       => $data['login'],
+        'details'     => [
+          'company'   => $data['company'],
+          'location'  => $data['location'],
+          'joined_on' => 'Joined on ' . (new \DateTime($data['created_at']))->format('d m Y'),
+        ],
+      ];
 
-      return array(
 
-
-      );
-
-//was testing the controller helloworld
+      //was testing the controller helloworld
       //      return array(
 //        '#title' => 'Hello World!',
 //        '#markup' => 'Here is some content.',
 //      );
+
     }
 
   }
