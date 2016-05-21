d84b4cfac0550175fa00d710bf82e95fb33f9999
Loyiso Ngqwebo <loyiso.ngqwebo@quirk.biz>
2016-05-21 15:01:54 +0200

    code refractoring

--- modules/apidisplay/src/Controller/ApidisplayController.php
+++ modules/apidisplay/src/Controller/ApidisplayController.php
@@ -1,35 +1,35 @@
 <?php
 
-namespace Drupal\apidisplay\Controller;
+  namespace Drupal\apidisplay\Controller;
 
   use Symfony\Component\HttpFoundation\Request;
 
+  class ApidisplayController {
 
-  class ApidisplayController  {
+    public function index(Request $request) {
 
-    public function index (Request $request){
-
-      $client = new \GuzzleHttp\Client;
+      $client   = new \GuzzleHttp\Client;
       $response = $client->request('GET', 'https://github.com/loyison');
 
-      $data = json_decode($response->getBody()->getContents(), true);
+      // get the github json fields to display
+
+      $data = json_decode($response->getBody()->getContents(), TRUE);
       return [
-        'avatar_url'  => $data['avatar_url'],
-        'name'        => $data['name'],
-        'login'       => $data['login'],
-        'details'     => [
+        'avatar_url' => $data['avatar_url'],
+        'name'       => $data['name'],
+        'login'      => $data['login'],
+        'details'    => [
           'company'   => $data['company'],
           'location'  => $data['location'],
           'joined_on' => 'Joined on ' . (new \DateTime($data['created_at']))->format('d m Y'),
         ],
       ];
 
-
       //was testing the controller helloworld
       //      return array(
-//        '#title' => 'Hello World!',
-//        '#markup' => 'Here is some content.',
-//      );
+      //        '#title' => 'Hello World!',
+      //        '#markup' => 'Here is some content.',
+      //      );
 
     }
 
