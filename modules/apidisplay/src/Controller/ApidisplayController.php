4cf27b12cdf290b05365e458ca62ce1549a395c2
Loyiso Ngqwebo <loyiso.ngqwebo@quirk.biz>
2016-05-21 14:34:16 +0200

    renamed folder

--- modules/apidisplay/src/Controller/ApidisplayController.php
+++ modules/apidisplay/src/Controller/ApidisplayController.php
@@ -1,18 +1,22 @@
 <?php
 
-namespace Drupal\apidisplay\Contoller;
+namespace Drupal\apidisplay\src\Controller;
 
   use Symfony\Component\HttpFoundation\Request;
 
-  class ApidisplayController {
+
+  class ApidisplayController  {
 
     public function index(Request $request){
 
-  $client = new \GuzzleHttp\Client;
-  $response = $client->request('GET', 'https://github.com/loyison');
+      $client = new \GuzzleHttp\Client;
+      $response = $client->request('GET', 'https://github.com/loyison');
       $data = json_decode($response->getBody()->getContents(), true);
 
+      return array(
 
+
+      );
 
 //was testing the controller helloworld
       //      return array(
